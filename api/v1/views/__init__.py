564a95f390ab7c46456520b49f3a581296700799
Erickkaranja <karanja99erick@gmail.com>
2023-08-02 21:59:30 +0300

    adding doc

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-from flask import Blueprint
+'''contains the blueprint of our application.'''
 
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,5 +11,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.place_reviews import *
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
