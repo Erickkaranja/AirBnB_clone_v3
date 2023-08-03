8c509e536ed907e5ca5115ca6067353d173ae10b
Erickkaranja <karanja99erick@gmail.com>
2023-08-03 16:30:25 +0300

    updating docs

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,6 @@
 '''contains the blueprint of our application and all imports to our objects.'''
 
 from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,3 +10,5 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.place_reviews import *
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
